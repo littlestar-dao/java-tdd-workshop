b7738133ea40b9ed7f8ca176f6e3fc58667aa97d
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 15:10:09 +0700

    if number=1 return [],other number return themself

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number) {
         List<Integer> list = new ArrayList<Integer>();
-        if(number == 2){
+        if(number != 1){
             list.add(number);
         }
         return list;
