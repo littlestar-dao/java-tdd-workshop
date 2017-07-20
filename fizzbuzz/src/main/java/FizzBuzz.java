5a75041e8e560c65c23f7accf23f33dd278e5153
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 10:47:33 +0700

    if input=5 => return Buzz

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -2,6 +2,9 @@
 public class FizzBuzz {
     public String say(int input) {
         if(input == 3){ return "Fizz";}
+        else if(input == 5){
+            return "Buzz";
+        }
         return input+"";
     }
 }
