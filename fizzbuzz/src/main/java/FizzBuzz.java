ef16d0c2958e7998c322556c2297ea930d5b8af5
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 10:49:30 +0700

    should_say_Fizz_when_number_is_6() is done

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,7 +1,7 @@
 
 public class FizzBuzz {
     public String say(int input) {
-        if(input == 3){ return "Fizz";}
+        if(input == 3 || input == 6){ return "Fizz";}
         else if(input == 5){
             return "Buzz";
         }
