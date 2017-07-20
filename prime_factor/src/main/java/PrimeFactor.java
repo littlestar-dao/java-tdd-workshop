254e3482ada04d71dcdc32662a19fa963f8f336e
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 15:43:03 +0700

    public void should_return_2_2_2_when_number_is_8() {

--- prime_factor/src/main/java/PrimeFactor.java
+++ prime_factor/src/main/java/PrimeFactor.java
@@ -7,14 +7,19 @@ public class PrimeFactor {
         if(number % 2 == 0){
             list.add(2);
             int divide = number/2;
-            if(divide > 1){
+            if(divide % 2 == 0 && divide > 2){ //8
+                list.add(divide/2);
+                if(divide/2 == 2){
+                    list.add(2);
+                }
+            }
+            else if(divide > 1){ //6, 4
                 list.add(divide);
             }
-
         }
-        else if(number != 1 && number % 2 != 0){
+        else if(number != 1 && number % 2 != 0){ //3
             list.add(number);
         }
-        return list;
+        return list; //1,2
     }
 }
