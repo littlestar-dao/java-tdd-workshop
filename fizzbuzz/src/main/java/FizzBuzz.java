968499ec6818d92e576fd59194ac979e659159e3
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 11:05:12 +0700

    if input= 15 or 30 => return FizzBuzz

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -1,13 +1,13 @@
 
 public class FizzBuzz {
     public String say(int input) {
-        if(input == 15){
+        if(input == 15 || input == 30){
             return "FizzBuzz";
         }
         if(input % 3 == 0) {
             return "Fizz";
         }
-        else if(input == 5 || input == 10 || input == 20){
+        else if(input % 5 == 0){
             return "Buzz";
         }
         return input+"";
