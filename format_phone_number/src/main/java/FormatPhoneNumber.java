19c05370624c7e95d6bebf24db28481a0715579e
littlestar-dao <dao.duan@hotmail.com>
2017-07-20 13:40:33 +0700

    add dash to evenPhoneNumber

--- format_phone_number/src/main/java/FormatPhoneNumber.java
+++ format_phone_number/src/main/java/FormatPhoneNumber.java
@@ -14,8 +14,26 @@ public class FormatPhoneNumber {
         return output;
     }
 
-    public String solution(String input) {
-        return null;
+    public String solution(String input){
+        String result = "";
+        int count = 0;
+        input = removeNonDigit(input);
+        char[] numbers = input.toCharArray();
+
+        for(int i = 0; i < numbers.length; i++)
+        {
+            int value1 = Character.getNumericValue(numbers[i]);
+            if(count < 3) {
+                count++;
+                result += value1;
+
+            }else{
+                count = 1;
+                result += "-";
+                result += value1;
+            }
+        }
+        return result;
     }
 
 }
